a37ed7d4d072765d4d70955bd850462e4a6be786
LucianoVazquezVargas <granlucho@hotmail.com>
2022-09-06 09:53:48 -0300

    Fin clase 20

--- coder/AppCoder/models.py
+++ coder/AppCoder/models.py
@@ -5,18 +5,22 @@ from django.db import models
 class Curso(models.Model):
     nombre=models.CharField(max_length=50)#str,maximo 50
     comision=models.IntegerField()#int
+    def __str__(self):
+        return self.nombre+" "+str(self.comision)
 
 class Estudiante(models.Model):
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
     email=models.EmailField()
-
+    def __str__(self):
+        return self.nombre+" "+str(self.apellido)
 class Profesor(models.Model):
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
     email=models.EmailField()
     profesion=models.CharField(max_length=50)
-
+    def __str__(self):
+        return self.nombre+" "+str(self.apellido)+" "+self.profesion
 class Entregable(models.Model):
     nombre=models.CharField(max_length=50)
     fecha_entrega=models.DateField()
